0cae0e0a440d400f55ed255649745da7ef8fb7de
Code Sandbox <hello@codesandbox.io>
2023-01-23 13:33:25 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1706aa3319d3fab70fbd9e1b308bb0ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7111c1ef8378df53eb3121212d17bfca",
+    "url": "https://hanikoussa.github.io/csb-rivvec/index.html"
+  },
+  {
+    "revision": "6505d2b8f1a2ed71e6e5",
+    "url": "https://hanikoussa.github.io/csb-rivvec/static/css/2.e1f817c4.chunk.css"
+  },
+  {
+    "revision": "e1e9457f710a8832f49d",
+    "url": "https://hanikoussa.github.io/csb-rivvec/static/css/main.d8799457.chunk.css"
+  },
+  {
+    "revision": "6505d2b8f1a2ed71e6e5",
+    "url": "https://hanikoussa.github.io/csb-rivvec/static/js/2.ae44ef4e.chunk.js"
+  },
+  {
+    "revision": "3bf1c66e928cc0041fec138534da2504",
+    "url": "https://hanikoussa.github.io/csb-rivvec/static/js/2.ae44ef4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e9457f710a8832f49d",
+    "url": "https://hanikoussa.github.io/csb-rivvec/static/js/main.5fa8e5d6.chunk.js"
+  },
+  {
+    "revision": "289fb503283a53fea81a",
+    "url": "https://hanikoussa.github.io/csb-rivvec/static/js/runtime-main.a9e14785.js"
+  },
+  {
+    "revision": "72e8c96acdbc62c57a2bfb3f689c80a8",
+    "url": "https://hanikoussa.github.io/csb-rivvec/static/media/herfah.72e8c96a.jpeg"
+  },
+  {
+    "revision": "debddf8698f276e2e9e2f005bab8e991",
+    "url": "https://hanikoussa.github.io/csb-rivvec/static/media/peakpx.debddf86.jpeg"
+  }
+]);
